99a5806d58258fc7a088fe842e8341bf55f02ecb
doxxx <doxxx93@gmail.com>
2023-03-09 20:35:08 +0900

    initial commit

---
+++ src/main/java/me/doxxx/fakeworld/FakeworldApplication.java
@@ -0,0 +1,13 @@
+package me.doxxx.fakeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/doxxx/fakeworld/FakeworldApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doxxx.fakeworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
